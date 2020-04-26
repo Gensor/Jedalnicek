b873b08dd74a34a5428db1ea0c634e8a969bf810
Gensor <Gensor@users.noreply.github.com>
2020-04-26 15:21:55 +0200

    init

--- src/main/java/com/mycompany/jedalnicek/MainGUI.java
+++ src/main/java/com/mycompany/jedalnicek/MainGUI.java
@@ -5,6 +5,13 @@
  */
 package com.mycompany.jedalnicek;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author John
@@ -16,6 +23,7 @@ public class MainGUI extends javax.swing.JFrame {
      */
     public MainGUI() {
         initComponents();
+       
     }
 
     /**
@@ -28,8 +36,12 @@ public class MainGUI extends javax.swing.JFrame {
     private void initComponents() {
 
         jFrame1 = new javax.swing.JFrame();
-        jLabel1 = new javax.swing.JLabel();
-        jPanel1 = new javax.swing.JPanel();
+        jPanel_Main = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable_Main_tyzdenZoznam = new javax.swing.JTable();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable_Main_tyzdenNutrienty = new javax.swing.JTable();
+        jButton_Main_pridajRecept = new javax.swing.JButton();
 
         javax.swing.GroupLayout jFrame1Layout = new javax.swing.GroupLayout(jFrame1.getContentPane());
         jFrame1.getContentPane().setLayout(jFrame1Layout);
@@ -43,49 +55,166 @@ public class MainGUI extends javax.swing.JFrame {
         );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Jedalnicek");
         setBackground(new java.awt.Color(255, 255, 255));
 
-        jLabel1.setText("kbdaskhdbkajhbsdjkhvbaksjd");
+        jPanel_Main.setBackground(new java.awt.Color(255, 255, 255));
 
-        jPanel1.setBackground(new java.awt.Color(153, 255, 153));
+        jScrollPane2.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        jScrollPane2.setViewportBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 612, Short.MAX_VALUE)
+        jTable_Main_tyzdenZoznam.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        jTable_Main_tyzdenZoznam.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {"23:00", null, null, null, null, null, null, null},
+                {"22:00", null, null, null, null, null, null, null},
+                {"21:00", null, null, null, null, null, null, null},
+                {"20:00", null, null, null, null, null, null, null},
+                {"19:00", null, null, null, null, null, null, null},
+                {"18:00", null, null, null, null, null, null, null},
+                {"17:00", null, null, null, null, null, null, null},
+                {"16:00", null, null, null, null, null, null, null},
+                {"15:00", null, null, null, null, null, null, null},
+                {"14:00", null, null, null, null, null, null, null},
+                {"13:00", null, null, null, null, null, null, null},
+                {"12:00", null, null, null, null, null, null, null},
+                {"11:00", null, null, null, null, null, null, null},
+                {"10:00", null, null, null, null, null, null, null},
+                {"09:00", null, null, null, null, null, null, null},
+                {"08:00", null, null, null, null, null, null, null},
+                {"07:00", null, null, null, null, null, null, null},
+                {"06:00", null, null, null, null, null, null, null},
+                {"05:00", null, null, null, null, null, null, null},
+                {"04:00", null, null, null, null, null, null, null},
+                {"03:00", null, null, null, null, null, null, null},
+                {"02:00", null, null, null, null, null, null, null},
+                {"01:00", null, null, null, null, null, null, null},
+                {"00:00", null, null, null, null, null, null, null}
+            },
+            new String [] {
+                "cas", "Pondelok", "Utorok", "Streda", "Stvrtok", "Piatok", "Sobota", "Nedela"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, true, true, true, true, true, true, true
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jTable_Main_tyzdenZoznam.setColumnSelectionAllowed(true);
+        jTable_Main_tyzdenZoznam.setGridColor(new java.awt.Color(204, 204, 204));
+        jTable_Main_tyzdenZoznam.setRowHeight(32);
+        jTable_Main_tyzdenZoznam.setSelectionBackground(new java.awt.Color(51, 153, 255));
+        jTable_Main_tyzdenZoznam.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        jTable_Main_tyzdenZoznam.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        jTable_Main_tyzdenZoznam.setShowGrid(true);
+        jTable_Main_tyzdenZoznam.getTableHeader().setReorderingAllowed(false);
+        jScrollPane2.setViewportView(jTable_Main_tyzdenZoznam);
+        jTable_Main_tyzdenZoznam.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        if (jTable_Main_tyzdenZoznam.getColumnModel().getColumnCount() > 0) {
+            jTable_Main_tyzdenZoznam.getColumnModel().getColumn(0).setResizable(false);
+        }
+
+        jScrollPane1.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+
+        jTable_Main_tyzdenNutrienty.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {"Bielkoviny", null, null, null, null, null, null, null},
+                {"Sacharidy", null, null, null, null, null, null, null},
+                {"Tuky", null, null, null, null, null, null, null},
+                {"kcal", null, null, null, null, null, null, null}
+            },
+            new String [] {
+                "Makronutrienty", "Pondelok", "Utorok", "Streda", "Stvrtok", "Piatok", "Sobota", "Nedela"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jTable_Main_tyzdenNutrienty.setColumnSelectionAllowed(true);
+        jTable_Main_tyzdenNutrienty.setGridColor(new java.awt.Color(204, 204, 204));
+        jTable_Main_tyzdenNutrienty.setSelectionBackground(new java.awt.Color(255, 255, 255));
+        jTable_Main_tyzdenNutrienty.setSelectionForeground(new java.awt.Color(0, 0, 0));
+        jTable_Main_tyzdenNutrienty.setShowGrid(true);
+        jTable_Main_tyzdenNutrienty.getTableHeader().setReorderingAllowed(false);
+        jScrollPane1.setViewportView(jTable_Main_tyzdenNutrienty);
+        jTable_Main_tyzdenNutrienty.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+
+        jButton_Main_pridajRecept.setText("+");
+        jButton_Main_pridajRecept.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_Main_pridajReceptActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel_MainLayout = new javax.swing.GroupLayout(jPanel_Main);
+        jPanel_Main.setLayout(jPanel_MainLayout);
+        jPanel_MainLayout.setHorizontalGroup(
+            jPanel_MainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel_MainLayout.createSequentialGroup()
+                .addGap(51, 51, 51)
+                .addGroup(jPanel_MainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 980, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1))
+                .addGap(35, 35, 35)
+                .addComponent(jButton_Main_pridajRecept, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(39, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 289, Short.MAX_VALUE)
+        jPanel_MainLayout.setVerticalGroup(
+            jPanel_MainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel_MainLayout.createSequentialGroup()
+                .addGap(83, 83, 83)
+                .addGroup(jPanel_MainLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 414, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton_Main_pridajRecept, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(28, 28, 28)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(159, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(25, 25, 25)
-                        .addComponent(jLabel1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(36, 36, 36)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(94, Short.MAX_VALUE))
+            .addComponent(jPanel_Main, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(23, 23, 23)
-                .addComponent(jLabel1)
-                .addGap(39, 39, 39)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(63, Short.MAX_VALUE))
+                .addComponent(jPanel_Main, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton_Main_pridajReceptActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_Main_pridajReceptActionPerformed
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }//GEN-LAST:event_jButton_Main_pridajReceptActionPerformed
 
     /**
      * @param args the command line arguments
@@ -123,8 +252,12 @@ public class MainGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton_Main_pridajRecept;
     private javax.swing.JFrame jFrame1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel_Main;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable_Main_tyzdenNutrienty;
+    private javax.swing.JTable jTable_Main_tyzdenZoznam;
     // End of variables declaration//GEN-END:variables
 }
